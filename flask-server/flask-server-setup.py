76dcaaada4dc7af240bbf47e3cadd191b65ef0d7
himani-paliha <103306909+himani-paliha@users.noreply.github.com>
2023-04-06 05:57:21 +0530

    feat: save input images to file

--- flask-server/flask-server-setup.py
+++ flask-server/flask-server-setup.py
@@ -9,11 +9,14 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
-# input images
+# accept input images and save them to 'images' file
 @app.route('/', methods=['POST'])
 def predit():
     imagefile = request.files['imagefile']
-    
+    image_path = "./images/" + imagefile.filename
+    imagefile.save(image_path)
+
+    return render_template('index.html')
 
 
 if __name__ == "__main__":
