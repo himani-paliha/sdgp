98050a62eb8c4833f57be25ae0f108ce95c4101b
himani-paliha <103306909+himani-paliha@users.noreply.github.com>
2023-04-06 05:33:00 +0530

    feat: choose image to input

--- flask-server/flask-server-setup.py
+++ flask-server/flask-server-setup.py
@@ -1,12 +1,20 @@
 # -*- coding: utf-8 -*-
 
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, request
 
 app = Flask(__name__)
 
-@app.route('/')
+# display index.html
+@app.route('/', methods=['GET'])
 def index():
     return render_template('index.html')
 
+# input images
+@app.route('/', methods=['POST'])
+def predit():
+    imagefile = request.files['imagefile']
+    
+
+
 if __name__ == "__main__":
-    app.run(port="8989")
+    app.run(port="8989", debug=True)
